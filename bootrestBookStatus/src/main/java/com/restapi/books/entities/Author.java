3205d17b799f683df20274327e44c1638aa5d2be
akgupta5799 <21mcmc21@uohyd.ac.in>
2024-07-09 17:06:01 +0530

    Spring Boot Example

---
+++ bootrestBookStatus/src/main/java/com/restapi/books/entities/Author.java
@@ -0,0 +1,51 @@
+package com.restapi.books.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "authors")
+public class Author {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "author_id")
+	private int authorId;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "language_known")
+	private String language;
+	
+	public int getAuthorId() {
+		return authorId;
+	}
+	public void setAuthorId(int authorId) {
+		this.authorId = authorId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getLanguage() {
+		return language;
+	}
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+}
--- bootrestBookStatus/src/main/java/com/restapi/books/entities/Books.java
+++ bootrestBookStatus/src/main/java/com/restapi/books/entities/Books.java
@@ -1,10 +1,12 @@
 package com.restapi.books.entities;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -17,10 +19,12 @@ public class Books {
 	private int id;
 	@Column(name = "title_name")
 	private String title;
-	@Column(name = "author_name")
-	private String author;
 	
-	public Books(int id, String title, String author) {
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	private Author author;
+	
+	public Books(int id, String title, Author author) {
 		super();
 		this.id = id;
 		this.title = title;
@@ -48,11 +52,11 @@ public class Books {
 		this.title = title;
 	}
 
-	public String getAuthor() {
+	public Author getAuthor() {
 		return author;
 	}
 
-	public void setAuthor(String author) {
+	public void setAuthor(Author author) {
 		this.author = author;
 	}
 
